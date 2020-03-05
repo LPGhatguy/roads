75f39b62093dd4746ca8bc7566a8125d5783a1d5
Lucien Greathouse <me@lpghatguy.com>
2020-03-04 18:11:13 -0800

    Revitalize

--- .remodel/build.lua
+++
@@ -1,7 +0,0 @@
-local contents = remodel.readFile("default.project.json")
-local project = json.fromString(contents)
-project.fileLocation = "default.project.json"
-
-local game = rojo.buildProject(project)
-
-remodel.writePlaceFile(game, "Place.rbxlx")
